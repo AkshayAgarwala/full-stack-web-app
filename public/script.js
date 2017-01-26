2944b04da3ebb40b7956824201f43a65fff0a3c8
Akshay Agarwala <akshay.agarwala2010@gmail.com>
2017-01-26 05:37:53 -0500

    Testing pic route

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
-window.setTimeout(function(){
-  $(document).ready(function(){
-      $("#weatherStart").fadeTo(2000, .8, function(){
-        $("#weatherEnd").fadeTo(1000, .8, function(){
-            $("#weatherMessage").animate({
-              opacity: '.7',
-              left: '40%'
-            }, 2000);
-        });
-      })
-  })
-}, 3000);
+// window.setTimeout(function(){
+//   $(document).ready(function(){
+//       $("#weatherStart").fadeTo(2000, .8, function(){
+//         $("#weatherEnd").fadeTo(1000, .8, function(){
+//             $("#weatherMessage").animate({
+//               opacity: '.7',
+//               left: '40%'
+//             }, 2000);
+//         });
+//       })
+//   })
+// }, 3000);
